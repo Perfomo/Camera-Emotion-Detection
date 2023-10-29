ecda0cd6b09b4288438499cf8366109e9093bce3
Perfomo <31494.perfomo@gmail.com>
2023-10-29 22:02:23 +0300

    create img

--- CameraApp/LaunchCamera.py
+++ CameraApp/LaunchCamera.py
@@ -1,9 +1,21 @@
 import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
 
 window = tk.Tk()
 
+cap = cv2.VideoCapture(0)
+cap.set(3, 1920)
+cap.set(4, 1080)
+
+rval, frame = cap.read()
+print(frame.shape)
+cap.release()
+
 window.geometry("1920x1080")
-window.maxsize(1920, 1080)  
-window.minsize(600, 450)
+window.minsize(960, 540)
+img = ImageTk.PhotoImage(image=Image.fromarray(frame))
+label = tk.Label(image=img)
+label.pack(expand=True)
 
 window.mainloop()
